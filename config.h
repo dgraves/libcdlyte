075049132165f609862b2b52bf97a88d5e916d9a
Dustin Graves <dgraves@computer.org>
2002-07-08 03:30:16 +0000

    config.h is updated.

--- config.h
+++ config.h
@@ -1,22 +1,5 @@
 /* config.h.  Generated automatically by configure.  */
 /* config.h.in.  Generated automatically from configure.in by autoheader.  */
-
-/* Define to empty if the keyword does not work.  */
-/* #undef const */
-
-/* Define if you have the getmntent function.  */
-#define HAVE_GETMNTENT 1
-
-/* Define if you have the ANSI C header files.  */
-#define STDC_HEADERS 1
-
-/* Define if you can safely include both <sys/time.h> and <time.h>.  */
-#define TIME_WITH_SYS_TIME 1
-
-/* Define if your processor stores words with the most significant
-   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
-/* #undef WORDS_BIGENDIAN */
-
 /* This symbol will be declared if we're compiling for an Irix system,
 and we have access to the Irix cdaudio library to do the dirty work. */
 /* #undef IRIX_CDLYTE */
@@ -25,81 +8,123 @@ and we have access to the Irix cdaudio library to do the dirty work. */
 /* #undef SOLARIS_GETMNTENT */
 /* #undef BROKEN_SOLARIS_LEADOUT */
 
+#define PACKAGE "libcdlyte"
+#define VERSION "0.9.8"
+
 /* Define if your system provides POSIX.4 threads */
 /* #undef HAVE_PTHREAD */
 
-/* The number of bytes in a long.  */
-#define SIZEOF_LONG 4
-
-/* Define if you have the gethostbyname function.  */
-#define HAVE_GETHOSTBYNAME 1
-
-/* Define if you have the gethostbyname_r function.  */
-#define HAVE_GETHOSTBYNAME_R 1
-
-/* Define if you have the getmntinfo function.  */
-/* #undef HAVE_GETMNTINFO */
-
-/* Define if you have the mkdir function.  */
-#define HAVE_MKDIR 1
-
-/* Define if you have the snprintf function.  */
-#define HAVE_SNPRINTF 1
-
-/* Define if you have the socket function.  */
-#define HAVE_SOCKET 1
-
-/* Define if you have the strerror function.  */
-#define HAVE_STRERROR 1
-
-/* Define if you have the strstr function.  */
-#define HAVE_STRSTR 1
-
-/* Define if you have the strtol function.  */
-#define HAVE_STRTOL 1
-
-/* Define if you have the <dlfcn.h> header file.  */
+/* Define if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
-/* Define if you have the <fcntl.h> header file.  */
+/* Define if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
 
-/* Define if you have the <io/cam/cdrom.h> header file.  */
+/* Define if you have the `gethostbyname' function. */
+#define HAVE_GETHOSTBYNAME 1
+
+/* Define if you have the `gethostbyname_r' function. */
+#define HAVE_GETHOSTBYNAME_R 1
+
+/* Define if you have the `getmntent' function. */
+#define HAVE_GETMNTENT 1
+
+/* Define if you have the `getmntinfo' function. */
+/* #undef HAVE_GETMNTINFO */
+
+/* Define if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if you have the <io/cam/cdrom.h> header file. */
 /* #undef HAVE_IO_CAM_CDROM_H */
 
-/* Define if you have the <linux/cdrom.h> header file.  */
+/* Define if you have the <linux/cdrom.h> header file. */
 #define HAVE_LINUX_CDROM_H 1
 
-/* Define if you have the <linux/ucdrom.h> header file.  */
+/* Define if you have the <linux/ucdrom.h> header file. */
 /* #undef HAVE_LINUX_UCDROM_H */
 
-/* Define if you have the <mntent.h> header file.  */
+/* Define if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if you have the `mkdir' function. */
+#define HAVE_MKDIR 1
+
+/* Define if you have the <mntent.h> header file. */
 #define HAVE_MNTENT_H 1
 
-/* Define if you have the <stdarg.h> header file.  */
+/* Define if you have the `snprintf' function. */
+#define HAVE_SNPRINTF 1
+
+/* Define if you have the `socket' function. */
+#define HAVE_SOCKET 1
+
+/* Define if you have the <stdarg.h> header file. */
 #define HAVE_STDARG_H 1
 
-/* Define if you have the <sys/cdio.h> header file.  */
+/* Define if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define if you have the `strtol' function. */
+#define HAVE_STRTOL 1
+
+/* Define if you have the <sys/cdio.h> header file. */
 /* #undef HAVE_SYS_CDIO_H */
 
-/* Define if you have the <sys/ioctl.h> header file.  */
+/* Define if you have the <sys/ioctl.h> header file. */
 #define HAVE_SYS_IOCTL_H 1
 
-/* Define if you have the <sys/mntent.h> header file.  */
+/* Define if you have the <sys/mntent.h> header file. */
 /* #undef HAVE_SYS_MNTENT_H */
 
-/* Define if you have the <sys/mount.h> header file.  */
+/* Define if you have the <sys/mount.h> header file. */
 #define HAVE_SYS_MOUNT_H 1
 
-/* Define if you have the <sys/ucred.h> header file.  */
+/* Define if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define if you have the <sys/ucred.h> header file. */
 /* #undef HAVE_SYS_UCRED_H */
 
-/* Define if you have the <unistd.h> header file.  */
+/* Define if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
 /* Name of package */
 #define PACKAGE "libcdlyte"
 
-/* Version number of package */
-#define VERSION "0.9.6"
+/* The size of a `long', as computed by sizeof. */
+#define SIZEOF_LONG 4
 
+/* Define if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* Version number of package */
+#define VERSION "0.9.8"
+
+/* Define if your processor stores words with the most significant byte first
+   (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
